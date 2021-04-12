b3b6db0c9493555f9f86f23f868368b5230a07be
Nomad <pamirkantar21@gmail.com>
2021-04-12 12:32:07 -0700

    inital boilerplate

---
+++ binance_coin_futures_api.go
@@ -0,0 +1,172 @@
+package go_binance
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"github.com/sirupsen/logrus"
+	"golang.org/x/net/http2"
+	"io"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"net/url"
+	"strconv"
+	"time"
+)
+
+type BinanceCoinFuturesApi struct {
+	Client *http.Client
+	BaseUrl string
+	PublicKey string
+	SecretKey string
+	Logger *logrus.Logger
+}
+
+func (bcfa *BinanceCoinFuturesApi) SetApiKeys(public, secret string) {
+	bcfa.PublicKey = public
+	bcfa.SecretKey = secret
+}
+
+func (bcfa *BinanceCoinFuturesApi) NewNetClient() {
+	// Todo: make sure timeout and handshake won't cause any problems.
+	var netTransport = &http.Transport{
+		DialContext: (&net.Dialer{
+			Timeout: 2 * time.Second,
+		}).DialContext,
+		DisableKeepAlives: false,
+		TLSHandshakeTimeout: 2 * time.Second,
+		ForceAttemptHTTP2:     true,
+	}
+	bcfa.Client = &http.Client{
+		Timeout:   0,
+		Transport: netTransport,
+	}
+}
+
+func (bcfa *BinanceCoinFuturesApi) NewNetClientHTTP2() {
+	var netTransport = &http2.Transport{}
+	bcfa.Client = &http.Client{
+		Timeout:   0,
+		Transport: netTransport,
+	}
+}
+
+func(bcfa *BinanceCoinFuturesApi) UseMainNet() {
+	bcfa.BaseUrl = mainNetBaseURL
+}
+func(bcfa *BinanceCoinFuturesApi) UseTestNet() {
+	bcfa.BaseUrl = testNetBaseURL
+}
+
+// Adds timestamp and creates a signature according to binance rules
+func (bcfa BinanceCoinFuturesApi) signParameters(parameters *url.Values) string {
+	tonce := strconv.FormatInt(time.Now().UnixNano(), 10)[0:13]
+	parameters.Add("timestamp", tonce)
+	signature, _ := bcfa.getSha256Signature(parameters.Encode())
+	return signature
+}
+
+// Signs given parameter values
+func (bcfa BinanceCoinFuturesApi) getSha256Signature(parameters string) (string, error) {
+	mac := hmac.New(sha256.New, []byte(bcfa.SecretKey))
+	_, err := mac.Write([]byte(parameters))
+	if err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(mac.Sum(nil)), nil
+}
+
+func (bcfa BinanceCoinFuturesApi) parseResponseBody(body io.ReadCloser) ([]byte, error) {
+	data, err := ioutil.ReadAll(body)
+	if err != nil {
+		bcfa.Logger.Println("Something went wrong during reading request body, ", err)
+	}
+	return data, nil
+}
+
+func (bcfa BinanceCoinFuturesApi) doPublicRequest(httpVerb, endPoint string, parameters url.Values) ([]byte, error) {
+	fullURL := bcfa.BaseUrl + endPoint
+	if parameters != nil {
+		fullURL += "?" + parameters.Encode()
+	}
+	request, _ := http.NewRequest(httpVerb, fullURL, nil)
+	response, err := bcfa.Client.Do(request)
+	if err != nil {
+		// Failure to speak HTTP - Connectivity error
+		// Non 2XX status does not produce errors
+		bcfa.Logger.Error("Connectivity error while Client.Do ", err)
+		return nil, err
+	}
+	// Log rate limit for debug purposes
+	// Even if request results in non 2xx status it provides
+	// rate limit information
+	bcfa.Logger.Println(endPoint + ", rate limit used: ",
+		response.Header.Get("X-Mbx-Used-Weight-1m"))
+
+	data, err := bcfa.parseResponseBody(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	// Non 2xx status does not return error
+	if response.StatusCode != 200 {
+		bem := new(BinanceErrorMessage)
+		err = json.Unmarshal(data, &bem)
+		if err != nil {
+			bem = nil
+		}
+		err = &RequestError{
+			StatusCode: response.StatusCode,
+			UrlUsed: 	fullURL,
+			Message:   	*bem,
+		}
+		bcfa.Logger.Error(endPoint, err.Error())
+		return nil, err
+	}
+	return data, nil
+}
+
+func (bcfa BinanceCoinFuturesApi) doSignedRequest(httpVerb, endPoint string, parameters url.Values) ([]byte, error) {
+	signature := bcfa.signParameters(&parameters)
+	headers := make(http.Header)
+	headers.Add("X-MBX-APIKEY", bcfa.PublicKey)
+	fullURL := bcfa.BaseUrl + endPoint + "?" + parameters.Encode() +"&signature=" + signature
+	request, _ := http.NewRequest(httpVerb, fullURL, nil)
+	request.Header = headers
+	response, err := bcfa.Client.Do(request)
+	if err != nil {
+		// Failure to speak HTTP - Connectivity error
+		// Non 2XX status does not produce errors
+		bcfa.Logger.Error("Connectivity error while Client.Do ", err)
+		return nil, err
+	}
+	// Log rate limit for debug purposes
+	// Even if request results in non 2xx status it provides
+	// rate limit information
+	bcfa.Logger.Println(endPoint + ", rate limit used: ",
+		response.Header.Get("X-Mbx-Used-Weight-1m"))
+
+	data, err := bcfa.parseResponseBody(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	// Non 2xx status does not return error
+	if response.StatusCode != 200 {
+		bem := new(BinanceErrorMessage)
+		err = json.Unmarshal(data, &bem)
+		if err != nil {
+			bem = nil
+		}
+		err = &RequestError{
+			StatusCode: response.StatusCode,
+			UrlUsed: 	fullURL,
+			Message:   	*bem,
+		}
+		bcfa.Logger.Error(endPoint, err.Error())
+		return nil, err
+	}
+	return data, nil
+}
